1fa3dc938dda5163a287b26777b91d7ec69a3a27
rizkaulee <rizka.rafi@gmail.com>
2020-06-10 19:17:01 +0700

    tambahan hlm login

---
+++ application/views/absensi/login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- <link rel="icon" href="../../../../favicon.ico"> -->
+
+	<title>Login</title>
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- css buatan sendiri -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/dist/css/login.css">
+</head>
+
+<body class="text-center">
+	<form class="form-signin">
+		<img class="mb-4" src="<?php echo base_url(); ?>assets/dist/img/logopnj.png" alt="" width="72" height="72">
+		<h1 class="h3 mb-3 font-weight-normal">Masuk Sebagai Mahasiswa</h1>
+		<label for="inputEmail" class="sr-only">Username</label>
+		<input type="email" id="inputEmail" class="form-control" placeholder="Masukkan username" required autofocus>
+		<label for="inputPassword" class="sr-only">Password</label>
+		<input type="password" id="inputPassword" class="form-control" placeholder="Masukkan password" required>
+		<div class="checkbox mb-3">
+			<label>
+				<input type="checkbox" value="remember-me"> Ingat saya
+			</label>
+		</div>
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>
+		<p class="mt-5 mb-3 text-muted">Copyright &copy; 2020 Jurusan TIK. All rights reserved.</p>
+	</form>
+</body>
+
+</html>
