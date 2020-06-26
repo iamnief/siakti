7e53e1570fa2e56da2a862f234bfee600942f77d
unknown <ilham22hanif@gmail.com>
2020-06-27 01:44:10 +0700

    perbaruan di batal_kelas, mulai_kelas, absen_kelas, detail_kelas, dan kelas_selesai

--- application/controllers/Absensi_Dosen.php
+++ application/controllers/Absensi_Dosen.php
@@ -21,10 +21,10 @@ class Absensi_Dosen extends CI_Controller
 
 		$data['user'] = $this->user;
 		$nip_dosen = $this->user['nip'];
-		
+
 		$url_jadwal = 'jadwalkuliah/dosen/' . $nip_dosen . '/' . $data['tgl'];
 		$data['resp_jadwal']  = $this->customguzzle->getBasicToken($url_jadwal, 'application/json');
-		
+
 		$url_kelas_batal = 'jadwalkuliah/dosenbatal/' . $nip_dosen;
 		$data['resp_kelas_batal'] = $this->customguzzle->getBasicToken($url_kelas_batal, 'application/json');
 
@@ -46,58 +46,134 @@ class Absensi_Dosen extends CI_Controller
 		$url = 'jadwalkuliah/dosen/' . $nip_dosen . '/' . $data['tgl'];
 		$data['resp_jadwal']  = $this->customguzzle->getBasicToken($url, 'application/json');
 
-		$layout['jsbottom'] = $this->load->view('absensi/jsbottom/dsn_jdwl', $data, true);
+		$layout['jsbottom'] = $this->load->view('absensi/jsbottom/dsn_jdwl', '', true);
 		$layout['userType'] = 'dosen';
 		$layout['title'] = 'Absensi Dosen';
 		$layout['menuActive'] = 'jadwal';
-		$layout['pages'] = $this->load->view('absensi/dosen/jadwal', '', true);
+		$layout['pages'] = $this->load->view('absensi/dosen/jadwal', $data, true);
 		$this->load->view('absensi/master', array('main' => $layout));
 	}
 
 	public function akhiri_kelas()
 	{
+		$data = $this->input->post();
+		$layout['jsbottom'] = $this->load->view('absensi/jsbottom/dsn_akhiri_kls', '', true);
 		$layout['userType'] = 'dosen';
 		$layout['title'] = 'Absensi Dosen';
 		$layout['menuActive'] = 'jadwal';
-		$layout['pages'] = $this->load->view('absensi/dosen/akhiri_kelas', '', true);
+		$layout['pages'] = $this->load->view('absensi/dosen/akhiri_kelas', $data, true);
 		$this->load->view('absensi/master', array('main' => $layout));
+	}
+
+	public function kelas_selesai($kode, $materi, $pin_mhs, $nim_mhs)
+	{
+		$url_verif = 'mahasiswa/verif';
+		$verif = array(
+			'nim' => $nim_mhs,
+			'pin' => $pin_mhs
+		);
+		$resp_verif = $this->customguzzle->postBlank($url_verif, 'application/json', $verif);
+		$result_verif = json_decode($resp_verif['data']);
+
+		if ($result_verif->jumlah > 0) {
+			$url_materi = 'absensi';
+			$data_absensi['kd_absendsn'] = $kode;
+			$data_absensi['jam_keluar'] = date('H:i:s');
+			$data_absensi['materi'] = $materi;
+			$resp_materi = $this->customguzzle->putBlank($url_materi, 'application/json', $data_absensi);
+			$resp_data = json_decode($resp_materi['data']);
+			$resp_data = $resp_data->responseCode;
+			echo $resp_data;
+		} else {
+			echo "salah";
+		}
 	}
 
 	public function detail_kelas()
 	{
+		$data = $this->input->post();
+		$layout['jsbottom'] = $this->load->view('absensi/jsbottom/dsn_detail_kls', '', true);
 		$layout['userType'] = 'dosen';
 		$layout['title'] = 'Absensi Dosen';
 		$layout['menuActive'] = 'jadwal';
-		$layout['pages'] = $this->load->view('absensi/dosen/detail_kelas', '', true);
+		$layout['pages'] = $this->load->view('absensi/dosen/detail_kelas', $data, true);
 		$this->load->view('absensi/master', array('main' => $layout));
+	}
+
+	public function absen_kelas($kd_absendsn)
+	{
+		$url = 'absenmhs/absenkelas/' . $kd_absendsn;
+		$resp = $this->customguzzle->getBasicToken($url, 'application/json');
+		$data = json_decode($resp['data']);
+		$absenkls = array(
+			'data' => $data
+		);
+		echo (json_encode($absenkls));
 	}
 
 	public function kelas_pengganti()
 	{
+		$data = $this->input->post();
+		$layout['jsbottom'] = $this->load->view('absensi/jsbottom/dsn_kls_ganti', '', true);;
 		$layout['userType'] = 'dosen';
 		$layout['title'] = 'Absensi Dosen';
 		$layout['menuActive'] = 'dashboard';
-		$layout['pages'] = $this->load->view('absensi/dosen/kelas_pengganti', '', true);
+		$layout['pages'] = $this->load->view('absensi/dosen/kelas_pengganti', $data, true);
 		$this->load->view('absensi/master', array('main' => $layout));
 	}
 
 	public function mulai_kelas()
 	{
-		$url_mulai_kelas = 'absensi/';
-		$layout['userType'] = 'dosen';
-		$layout['title'] = 'Absensi Dosen';
-		$layout['menuActive'] = 'dashboard';
-		$layout['pages'] = $this->load->view('absensi/dosen/kelas_pengganti', '', true);
-		$this->load->view('absensi/master', array('main' => $layout));
+		$kodejdwl = $this->input->post('kodejdwl');
+		$tipe_kelas = $this->input->post('tipe_kelas');
+		$kodeklas = $this->input->post('kodeklas');
+		$toleransi_absen = '3 minutes';
+
+		//** menambahkan data pada tabel absensi */
+		$url_absensi = 'absensi/';
+		$data_absensi = array(
+			'tgl' => date('d-m-Y'),
+			'jam_msk' => date('H:i:s', strtotime($toleransi_absen, strtotime(date('H:i:s')))),
+			'staff_nip' => $this->user['nip']
+		);
+		if ($tipe_kelas == 'normal') {
+			$data_absensi['jadwal_kul_kodejdwl'] = $kodejdwl;
+		} else if ($tipe_kelas == 'pengganti') {
+			$data_absensi['kls_pengganti_kd_gantikls'] = $kodejdwl;
+		}
+		$resp1 = $this->customguzzle->postBlank($url_absensi, 'application/json', $data_absensi);
+		// echo json_encode($resp1);
+		// echo '<br>';
+
+		/* menyetel absen tiap mahasiswa di kelas dengan status alpha */
+		$url_absen_mhs = 'absenmhs/mulaikelas';
+		$data_absen_mhs = array(
+			'kodeklas' => $kodeklas,
+			'tgl' => date('d-m-Y'),
+		);
+		if ($tipe_kelas == 'normal') {
+			$data_absen_mhs['jadwal_kul_kodejdwl'] = $kodejdwl;
+			$data_absen_mhs['tipe_kelas'] = $tipe_kelas;
+		} else if ($tipe_kelas == 'pengganti') {
+			$data_absen_mhs['kls_pengganti_kd_gantikls'] = $kodejdwl;
+			$data_absen_mhs['tipe_kelas'] = $tipe_kelas;
+		}
+		$resp2 = $this->customguzzle->postBlank($url_absen_mhs, 'application/json', $data_absen_mhs);
+		// echo json_encode($resp2);
+
+		header("Location: " . site_url('absensi_dosen/'));
 	}
 
 	public function batal_kelas()
 	{
-		$url_mulai_kelas = 'absensi/';
-		$layout['userType'] = 'dosen';
-		$layout['title'] = 'Absensi Dosen';
-		$layout['menuActive'] = 'dashboard';
-		$layout['pages'] = $this->load->view('absensi/dosen/kelas_pengganti', '', true);
-		$this->load->view('absensi/master', array('main' => $layout));
+		$url_batalkelas = 'kelaspengganti/batalkelas';
+		$data = array(
+			'tgl_batal' => $this->input->post('tgl'),
+			'kode' => $this->input->post('kodejdwl'),
+			'tipe_kelas' => $this->input->post('tipe_kelas'),
+			'jml_jam' => $this->input->post('jml_jam')
+		);
+		$resp1 = $this->customguzzle->postBlank($url_batalkelas, 'application/json', $data);
+		header("Location: " . site_url('absensi_dosen/'));
 	}
 }
