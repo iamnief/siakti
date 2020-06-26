39efc8fd9c3c143d09fc9291886e56f66272e009
unknown <ilham22hanif@gmail.com>
2020-06-27 01:50:48 +0700

    javascript untuk dosen/akhiri_kelas

---
+++ application/views/absensi/jsbottom/dsn_akhiri_kls.php
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+    function onClickSelesai() {
+        var materi = $('#materi').val();
+        var pin_mhs = $('#pin_mhs').val();
+        var nim_mhs = $('#nim_mhs').val();
+        var kode = $('#kode').val();
+
+        if (materi === '' || nim_mhs === '' || pin_mhs === '') {
+            document.getElementById("message").innerHTML = "<p style='color: red; font-size: 12px;'>Silakan Isi Kolom yang masih kosong</p>";
+        } else {
+            var url = "<?php echo site_url('absensi_dosen/kelas_selesai') ?>"
+            + "/" +  kode + "/" +  materi + "/" + pin_mhs + "/" + nim_mhs;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var verif = this.responseText;
+                    if(verif == '00'){
+                        var display = "<div class='card-body'><h3 class='text-center'>Kelas Telah Selesai</h3>"
+                        display += "<a class='btn btn-yellow' href='<?php echo site_url('absensi_dosen/index') ?>'>Dashboard</a></div>";
+                        document.getElementById("form_verif").innerHTML = display;
+                    } else {
+                        document.getElementById("message").innerHTML = "<p style='color: red; font-size: 12px;'>NIM atau PIN salah</p>";
+                    }
+                }
+            };
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+        }
+    }
+</script>
