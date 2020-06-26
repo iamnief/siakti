ec9adb191e8ea3407628be3130fdea64142361a9
unknown <ilham22hanif@gmail.com>
2020-06-27 01:51:16 +0700

    javascript untuk dosen/kelas_pengganti

---
+++ application/views/absensi/jsbottom/dsn_kls_ganti.php
@@ -0,0 +1,8 @@
+<!-- <script type="text/javascript">
+  // The Calender
+  $(document).ready(function() {
+    $('#tanggal_pengganti').datetimepicker({
+      format: 'L'
+    });
+  });
+</script> -->
