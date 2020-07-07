c050d1142dbd69bcf215f873e01b7debeb9ff88e
unknown <ilham22hanif@gmail.com>
2020-07-08 02:09:33 +0700

    menampilkan data perkuliahan

--- application/views/absensi/kps/detail_perkuliahan.php
+++ application/views/absensi/kps/detail_perkuliahan.php
@@ -29,9 +29,9 @@
             <p><b>Kelas</b></p>
           </div>
           <div class="col-4">
-            <p>:Data Mining</p>
-            <p>:Euis</p>
-            <p>:TI - 6A</p>
+            <p>:<?php if (isset($perkuliahan['namamk'])) echo $perkuliahan['namamk'] ?></p>
+            <p>:<?php if (isset($perkuliahan['dosen'])) echo $perkuliahan['dosen'] ?></p>
+            <p>:<?php if (isset($perkuliahan['namaklas'])) echo $perkuliahan['namaklas'] ?></p>
           </div>
           <div class="col-2">
             <p><b>Tanggal</b></p>
@@ -39,27 +39,34 @@
             <p><b>Materi</b></p>
           </div>
           <div class="col-4">
-            <p>:14 Januari 2020</p>
-            <p>:Pengenalan</p>
-            <p>:Pengenalan</p>
+            <p>:<?php if (isset($perkuliahan['tgl'])) echo $perkuliahan['tgl'] ?></p>
+            <p>:<?php if (isset($perkuliahan['materi'])) echo $perkuliahan['materi'] ?></p>
+            <p>:<?php if (isset($perkuliahan['materi'])) echo $perkuliahan['materi'] ?></p>
           </div>
         </div>
       </div>
       <!-- /.card-header -->
       <div class="table-responsive p-0">
         <!-- form start -->
-        <form class="form-horizontal">
+        <form class="form-horizontal" method="post" action="<?php echo site_url('absensi_kps/verif_perkuliahan'); ?>">
           <div class="card-body text-center">
             <div>
-              <p>Masukkan PIN untuk Verifikasi Perkuliahan Ini</p>
+              <p>Catatan</p>
             </div>
             <div class="form-group">
-              <div>
-                <input type="password" class="form-control" name="pin" id="pin" placeholder="PIN">
-                <input type="text" class="form-control" name="kd_absendsn" id="kd_absendsn" hidden>
-              </div>
+              <!-- <label for="note_kps">Catatan</label> -->
+              <textarea name="note_kps" id="note_kps" cols="40" rows="5" maxlength="200" placeholder="Max 200 karakter"></textarea>
             </div>
-            <button type="submit" class="btn btn-yellow">Selesai</button>
+            <div>
+              <p>Masukkan PIN untuk Verifikasi</p>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" name="pin" id="pin" placeholder="PIN">
+              <input type="text" class="form-control" name="kd_absendsn" hidden id="kd_absendsn" value="<?php if (isset($perkuliahan['kd_absendsn'])) echo $perkuliahan['kd_absendsn'] ?>">
+            </div>
+            <p><span id="message"></span></p>
+            <a id="verif_perkuliahan" class="btn btn-yellow">Verifikasi</a>
+            <button id="submit_perkuliahan" type="submit" class="btn btn-yellow" hidden></button>
           </div>
           <!-- /.card-body -->
         </form>
