7d66d035e9b5193a73fcd3f55c069733a3af9838
unknown <ilham22hanif@gmail.com>
2020-07-08 02:08:31 +0700

    js untuk detail_perkuliahan

---
+++ application/views/absensi/jsbottom/kps_detail_perkuliahan.php
@@ -0,0 +1,28 @@
+<script>
+    const pin = document.getElementById("pin");
+    const btn_verif = document.getElementById("verif_perkuliahan");
+    const btn_submit = document.getElementById("submit_perkuliahan");
+    const message = document.getElementById("message");
+
+    btn_verif.onclick = () => {
+        if ($('#kd_absendsn').val() != '' && $('#kd_absendsn').val() != null) {
+            let url = "<?php echo site_url('absensi_kps/checkpin'); ?>";
+            let xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    if (this.responseText == 'true') {
+                        btn_submit.click();
+                    } else {
+                        message.innerText = 'Pin Salah';
+                        message.style = 'color: red;';
+                    }
+                }
+            };
+            xmlhttp.open("POST", url, true);
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.send(`pin=${pin.value}`);
+        } else {
+            message.innerText = 'Tak ada';
+        }
+    }
+</script>
