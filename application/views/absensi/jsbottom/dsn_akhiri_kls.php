fa186057138c688c9cb4ca687e55f5c6eca05cd1
unknown <ilham22hanif@gmail.com>
2020-07-08 02:07:12 +0700

    var diganti let

--- application/views/absensi/jsbottom/dsn_akhiri_kls.php
+++ application/views/absensi/jsbottom/dsn_akhiri_kls.php
@@ -1,21 +1,21 @@
 <script type="text/javascript">
     function onClickSelesai() {
-        var materi = $('#materi').val();
-        var pin_mhs = $('#pin_mhs').val();
-        var nim_mhs = $('#nim_mhs').val();
-        var kode = $('#kode').val();
+        let materi = $('#materi').val();
+        let pin_mhs = $('#pin_mhs').val();
+        let nim_mhs = $('#nim_mhs').val();
+        let kode = $('#kode').val();
 
         if (materi === '' || nim_mhs === '' || pin_mhs === '') {
             document.getElementById("message").innerHTML = "<p style='color: red; font-size: 12px;'>Silakan Isi Kolom yang masih kosong</p>";
         } else {
-            var url = "<?php echo site_url('absensi_dosen/kelas_selesai') ?>"
+            let url = "<?php echo site_url('absensi_dosen/kelas_selesai') ?>"
             + "/" +  kode + "/" +  materi + "/" + pin_mhs + "/" + nim_mhs;
-            var xmlhttp = new XMLHttpRequest();
+            let xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    var verif = this.responseText;
+                    let verif = this.responseText;
                     if(verif == '00'){
-                        var display = "<div class='card-body'><h3 class='text-center'>Kelas Telah Selesai</h3>"
+                        let display = "<div class='card-body'><h3 class='text-center'>Kelas Telah Selesai</h3>"
                         display += "<a class='btn btn-yellow' href='<?php echo site_url('absensi_dosen/index') ?>'>Dashboard</a></div>";
                         document.getElementById("form_verif").innerHTML = display;
                     } else {
